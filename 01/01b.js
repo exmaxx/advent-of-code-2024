4660be2c61a9874461cc774e6a58ab3e12663d38
Marek Ulicny <xulicny@gmail.com>
2024-12-16 12:50:35 +0100

    day 1b - add test

--- 01/01b.js
+++ 01/01b.js
@@ -1,33 +1,36 @@
 import { readFromFile } from '../utils.js'
 
-// const lines = readFromFile('01-input.txt')
-const lines = readFromFile('01-example.txt')
+export function solve (filePath) {
+  const lines = readFromFile(filePath)
 
-let sum = 0
-const firsts = []
-const seconds = []
+  let sum = 0
+  const firsts = []
+  const seconds = []
 
-for (const line of lines) {
-  const regex = /\d+/g
-  const [first, second] = line.match(regex)
+  for (const line of lines) {
+    const regex = /\d+/g
+    const [first, second] = line.match(regex)
 
-  firsts.push(Number(first))
-  seconds.push(Number(second))
+    firsts.push(Number(first))
+    seconds.push(Number(second))
+  }
+
+  // const firstCounts = {}
+  const secondCounts = {}
+
+  for (let i = 0; i < lines.length; i++) {
+    const numberToCheck = seconds[i]
+    secondCounts[numberToCheck] = secondCounts[numberToCheck]
+      ? secondCounts[numberToCheck] + 1
+      : 1
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    const numberToCheck = firsts[i]
+    const count = secondCounts[numberToCheck] || 0
+
+    sum += numberToCheck * count
+  }
+
+  return sum
 }
-
-// const firstCounts = {}
-const secondCounts = {}
-
-for (let i = 0; i < lines.length; i++) {
-  const numberToCheck = seconds[i]
-  secondCounts[numberToCheck] = secondCounts[numberToCheck] ? secondCounts[numberToCheck] + 1 : 1
-}
-
-for (let i = 0; i < lines.length; i++) {
-  const numberToCheck = firsts[i]
-  const count = secondCounts[numberToCheck] || 0
-
-  sum += numberToCheck * count
-}
-
-console.log('sum:', sum)
